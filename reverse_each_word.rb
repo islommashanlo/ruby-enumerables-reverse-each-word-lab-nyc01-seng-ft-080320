4fff44cdcc806a9ec56524f85925ca491c6c146d
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-30 19:51:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word (string)
     array_s = string.split(/ /)
     array_s.each do |e|
-      reverse_array = []
       reversed_e = e.reverse
       reverse_array.push(reversed_e)
     end
